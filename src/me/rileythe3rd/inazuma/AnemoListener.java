93ab8d2ce03216c0c92786fa9d507a03dfa4f1d4
Riley <pikachuassasin906@gmail.com>
2022-08-04 00:17:04 -0500

    Power makes player yeet in air and plunge with a right click.
    Also adds resistance during plunge

--- src/me/rileythe3rd/inazuma/AnemoListener.java
+++ src/me/rileythe3rd/inazuma/AnemoListener.java
@@ -1,14 +1,37 @@
 package me.rileythe3rd.inazuma;
 
+import java.util.HashMap;
+
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.util.Vector;
 
 public class AnemoListener implements Listener{
+	@EventHandler
+	public void onPlayerLeftClick(PlayerInteractEvent event) {
+		Player p = event.getPlayer();
+		Action a = event.getAction();
+		//If player is in map, drag them down and do damage, player will be removed in  2 seconds
+		if ((a == Action.LEFT_CLICK_AIR || a == Action.LEFT_CLICK_BLOCK) && map.containsKey(p)) {
+			p.setVelocity(new Vector(0, -40, 0));
+			
+			//give player resistance during plunge
+			p.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 2 * 20, 5));
+			map.remove(p).cancel();
+		}
+	}
+	
+	public HashMap<Player, BukkitTask> map = new HashMap<>();
 	
 	@EventHandler
 	public void onPlayerSneak(PlayerToggleSneakEvent event) {
@@ -19,15 +42,22 @@ public class AnemoListener implements Listener{
 			if (is != null && is.hasItemMeta()) {
 				ItemMeta im = is.getItemMeta();
 				if (im.getLore().contains("Anemo Vision")) {
-					p.setVelocity(new Vector(15, 0, 0));
-					
 					//Yeet in the air: slight velocity up?
+					p.setVelocity(new Vector(0, 40, 0));
+					map.put(p, 
+					
+					//remove added player after 2 seconds
+					Bukkit.getScheduler().runTaskLater(InazumaPlugin.getPlugin(), () -> {
+					    map.remove(p);
+					}, 2 * 20)
+				
+					);
 					//When click in mid air, plunge
 					//if no click in midair, just float down
 					//invulnerable for a certain duration (what if they disconnect)
-				}
+				
 			}
 		}
 	}
-
+	}
 }
